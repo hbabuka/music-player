614b2c243c717a23dcab33f084a4cdf00fbde4c3
Hristijan Babuka <hristijan.babuka@gmail.com>
2022-03-06 11:33:59 +0100

    Styling the input range

--- src/App.js
+++ src/App.js
@@ -15,12 +15,23 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   const handleTimeUpdate = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animationPercentage = Math.round(
+      (roundedCurrent / roundedDuration) * 100
+    );
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration: duration,
+      animationPercentage: animationPercentage,
+    });
   };
   return (
     <div className="App">
